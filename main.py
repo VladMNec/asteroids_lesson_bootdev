466d94ce7e87502cac9317a8552fa7edcf4f0b32
VM_N <necvladm@gmail.com>
2025-02-22 19:26:02 +0200

    start project

---
+++ main.py
@@ -0,0 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+# import constants file
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    game_on = True
+    while game_on:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
