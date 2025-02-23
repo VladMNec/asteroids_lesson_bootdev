8dd81576bfe1485f0907a1cbfb4915e313e314e3
VM_N <necvladm@gmail.com>
2025-02-23 13:06:00 +0200

    add moving asteroid field

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ import pygame
 from constants import *
 # import player
 import player
+import asteroid
+import asteroidfield
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+asteroidfld = pygame.sprite.Group()
 
 def main():
     pygame.init()
@@ -19,7 +23,10 @@ def main():
     fps = pygame.time.Clock()
     dt = 0
     player.Player.containers = (updatable, drawable)
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
     player_sprite = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = asteroidfield.AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
